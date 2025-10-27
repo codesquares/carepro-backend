9deaafd8ce1e2c0f39217ea2976f70fa883b22d6
James Oluwatosin Fadeyi <jamesfa@HA-SHEM.com>
2025-10-27 13:46:42 +0100

    updated

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203d6ba904b562c163f170b6073b89a92eff4846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57ccd4ee4ae57fbbe025b105c3c2def2bb27574")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+203d6ba904b562c163f170b6073b89a92eff4846")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57ccd4ee4ae57fbbe025b105c3c2def2bb27574")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
